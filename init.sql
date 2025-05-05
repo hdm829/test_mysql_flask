1459f517b90e87afbdb2eb655343025f8e19805b
hdm829 <daeman.han@gmail.com>
2025-05-05 17:58:42 +0900

    Create init.sql

---
+++ init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS time_series_data (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    timestamp DATETIME,
+    feature1 FLOAT,
+    feature2 FLOAT,
+    feature3 FLOAT,
+    feature4 FLOAT,
+    feature51 FLOAT
+);
